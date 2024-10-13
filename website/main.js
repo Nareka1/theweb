79cd3d0e516abfd42b96745786e7f4da23718123
Nareka1 <134252479+Nareka1@users.noreply.github.com>
2024-10-13 20:16:28 +0700

    Add files via upload

---
+++ website/main.js
@@ -0,0 +1,45 @@
+
+
+
+// for(var i =0; i<3; i++){
+//     console.log('djamput')
+// }
+
+// let player = ('kertas');
+// let bot = ""[Math.floor(Math.random()*4)] ;
+// let akhir = "";
+// let jawab="";
+
+// if(bot==1){
+//     bot='batu'
+//     if(player=='kertas'){
+//         akhir="menang"
+//     }
+//     else if(player=='gunting'){
+//         akhir="kalah"
+//     }
+// }
+// else if(bot==2){
+//     bot='gunting'
+//     if(player=='batu'){
+//         akhir='menang'
+//     }
+//     else if(player=='kertas'){
+//         akhir='kalah'
+//     }
+// }
+// else{
+//     jawab='error'
+// }
+
+// console.log('bot menjawab',bot);
+// console.log('player menjawab:', player,'akhirnya', akhir)
+// let button = document.querySelector('button')
+// let input = document.querySelector('input')
+// let h1 = document.querySelector('h1')
+
+// button.addEventListener('click',function(){
+//     var isi= input.value;
+
+//     h1.innerHTML = isi
+// })
